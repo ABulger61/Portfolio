65665b33fef056ca0ec95449c563011e0a8069e8
ABulger61 <abulger1@gmail.com>
2016-12-05 23:47:18 -0500

    Add sidebar

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,15 @@
-$(document).(function() {
-Hello World!
+$(document).ready(function () {
+var $menu = $('#sidebar-wrapper');
+
+$(document)
+ .on('click', '.js-menu-open',function() {
+  $menu.addClass('open');
+
+  return false;
+})
+  .on('click', '.js-menu-close', function() {
+    $menu.removeClass('open');
+
+    return false;
+  });
 });
